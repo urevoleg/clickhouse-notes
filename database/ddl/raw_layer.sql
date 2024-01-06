76e1b5f8c3c0b68215a4ff10a506ebffb786f9e7
urevoleg <urevolegg@gmail.com>
2024-01-06 12:48:59 +0300

    add cdc.openweathermap_mv and target table

--- database/ddl/raw_layer.sql
+++ database/ddl/raw_layer.sql
@@ -8,3 +8,20 @@ record_value String
 ENGINE=MergeTree
 ORDER BY (record_timestamp)
 ;
+
+-- table for MV
+CREATE OR REPLACE TABLE cdc.openweathermap
+(
+	record_timestamp DateTime64,
+	dt DateTime,
+	main String,
+	description String,
+	temp Float64,
+	feels_like Float64,
+	pressure Float64,
+	humidity Float64
+)
+ENGINE=MergeTree
+PARTITION BY toStartOfWeek(record_timestamp)
+ORDER BY (main, description, record_timestamp)
+;
